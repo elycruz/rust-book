9782376b9a6702cb677f8c7e9648256d2ed7d969
Ely De La Cruz <elycruz@elycruz.com>
2019-09-29 15:04:05 -0400

    chp8-comp-dir - progress (added 'add' command).

--- chp8/company_directory.rs
+++ chp8/company_directory.rs
@@ -3,13 +3,13 @@ use std::collections::HashSet;
 use regex::Regex;
 use std::io;
 
-type Department<'a> = &'a str;
+type Department = String;
 
-type Employee<'a> = &'a str;
+type Employee = String;
 
-type Listings<'a> = HashMap<Department<'a>, HashSet<Employee<'a>>>;
+type Listings<'a> = HashMap<Department, HashSet<Employee>>;
 
-fn prompt_for_input(rx: &Regex) -> String {
+fn prompt_for_input_and_extract(rx: &Regex) -> Vec<String> {
     let mut input = String::new();
     let mut ind = 0;
     while !rx.is_match(input.trim()) {
@@ -26,37 +26,32 @@ fn prompt_for_input(rx: &Regex) -> String {
         }
         ind += 1;
     }
-    return input;
-}
-
-fn parse_input<'a, 'b>(rx: &'a Regex, input: &'b str) -> Vec<&'b str> {
-    let mut args: Vec<&'b str> = vec![];
+    let mut args: Vec<String> = vec![];
     let cs = rx.captures(input.trim()).unwrap();
     let parts = cs.get(0).unwrap().as_str().split(" ");
     for p in parts {
-        let s: &'b str = p;
-        args.push(s);
+        args.push(String::from(p));
     }
     args
 }
 
-fn main<'a, 'b>() {
+fn main() {
     let cmd_rx: Regex = regex::Regex::new(r"^(add|exit)(\s*[A-Za-z_]{2,55})+$").unwrap();
     let mut listings: Listings = HashMap::new();
     let mut quit = false;
     loop {
-        let input: String = prompt_for_input(&cmd_rx);
-        let args = parse_input(&cmd_rx, &input);
+        let args = prompt_for_input_and_extract(&cmd_rx);
         if args.len() == 0 {
             println!("No command given.  Doing nothing.");
             continue;
         }
-        match args[0] {
+        match args[0].as_str() {
             "add" => {
-                if args.len() >= 3 {
+                if args.len() < 3 {
                     println!("Unable to run 'add' command.  Need more args.  Received: {:?}", args);
                 } else {
-                    add_dept_emp(args[1], args[0], &mut listings)
+                    add_dept_emp(args[2].as_str(), args[1].as_str(), &mut listings);
+                    println!("{:?}", listings);
                 }
             }
             "exit" => {
@@ -73,8 +68,8 @@ fn main<'a, 'b>() {
 }
 
 fn add_dept_emp<'a, 'b>(dept_name: &'a str, emp_name: &'a str, hash_map: &'b mut Listings<'a>) {
-    hash_map.entry(dept_name).or_insert(HashSet::new())
-        .insert(emp_name)
+    hash_map.entry(dept_name.to_string()).or_insert(HashSet::new())
+        .insert(emp_name.to_string())
     ;
 }
 
@@ -85,8 +80,8 @@ mod test {
 
     #[test]
     fn test_add_dept_emp() {
-        let depts: Vec<Department> = vec!["business", "development", "marketing"];
-        let emps: Vec<Employee> = vec!["Emp 1", "Emp 2", "Emp 3"];
+        let depts: Vec<&str> = vec!["business", "development", "marketing"];
+        let emps: Vec<&str> = vec!["Emp 1", "Emp 2", "Emp 3"];
         let mut _listings: Listings = HashMap::new();
         let mut i: usize = 0;
         let mut j: usize = 0;
