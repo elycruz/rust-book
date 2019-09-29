f37ebc2a5b52a21e742bc9dd3c14c22f8b229022
Ely De La Cruz <elycruz@elycruz.com>
2019-09-29 02:49:40 -0400

    chp8-company_dir exercise progress (tests for add_emp_dept method, and some ui progress).

--- chp8/company_directory.rs
+++ chp8/company_directory.rs
@@ -1,32 +1,82 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
+use regex::Regex;
+use std::io;
 
-type Store<'a, 'b> = HashMap<&'a str, &'b mut Vec<&'a str>>;
+type Department<'a> = &'a str;
 
-fn add_employee<'a, 'b>(name: &'a str, dept_name: &'a str, store: &'a mut Store<'a, 'b>) {
-    add_dept(dept_name, store);
-    match store.get(dept_name) {
-        Some(xs) => {
-            (*xs).push(name);
+type Employee<'a> = &'a str;
+
+type Listings<'a> = HashMap<Department<'a>, HashSet<Employee<'a>>>;
+
+fn prompt_for_input(rx: &Regex) -> String {
+    let mut input = String::new();
+    let mut ind = 0;
+    while !rx.is_match(input.trim()) {
+        if ind > 0 { // if asked for input and regex match failed ...
+            println!("Wrong command format.  Should be `cmd [...args]`.");
+            input.clear();
         }
-        _ => ()
+        println!("\n$:directory>");
+        match io::stdin().read_line(&mut input) {
+            Err(e) => {
+                println!("Command error: {:?}", e);
+            }
+            Ok(_) => ()
+        }
+        ind += 1;
+    }
+    return input;
+}
+
+fn parse_input<'a, 'b>(rx: &'a Regex, input: &'b str) -> Vec<&'b str> {
+    let mut args: Vec<&'b str> = vec![];
+    let cs = rx.captures(input.trim()).unwrap();
+    let parts = cs.get(0).unwrap().as_str().split(" ");
+    for p in parts {
+        let s: &'b str = p;
+        args.push(s);
+    }
+    args
+}
+
+fn main<'a, 'b>() {
+    let cmd_rx: Regex = regex::Regex::new(r"^(add|exit)(\s*[A-Za-z_]{2,55})+$").unwrap();
+    let mut listings: Listings = HashMap::new();
+    let mut quit = false;
+    loop {
+        let input: String = prompt_for_input(&cmd_rx);
+        let args = parse_input(&cmd_rx, &input);
+        if args.len() == 0 {
+            println!("No command given.  Doing nothing.");
+            continue;
+        }
+        match args[0] {
+            "add" => {
+                if args.len() >= 3 {
+                    println!("Unable to run 'add' command.  Need more args.  Received: {:?}", args);
+                } else {
+                    add_dept_emp(args[1], args[0], &mut listings)
+                }
+            }
+            "exit" => {
+                quit = true
+            }
+            _ => {
+                println!("No command found.  Doing nothing");
+            }
+        }
+        if quit {
+            break;
+        }
     }
 }
 
-fn add_dept<'a, 'b>(dept_name: &'a str, store: &'b mut Store<'a, 'b>)  {
-    match store.get(dept_name) {
-        Some(_) => (),
-        _ => {
-            let xs: &mut Vec<&str> = &mut vec![];
-            store.insert(dept_name, xs);
-        }
-    }
+fn add_dept_emp<'a, 'b>(dept_name: &'a str, emp_name: &'a str, hash_map: &'b mut Listings<'a>) {
+    hash_map.entry(dept_name).or_insert(HashSet::new())
+        .insert(emp_name)
+    ;
 }
-
-//fn get_dept_directory(name: &str, store: &mut Store) {}
-//
-//fn get_company_directory() {}
-
-fn main() {}
 
 #[cfg(test)]
 mod test {
@@ -34,19 +84,22 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_add_employee() {}
-
-    #[test]
-    fn test_add_dept() {
-        let store: &mut Store = &mut HashMap::new();
-
-        add_dept("hello world", store);
-        assert_eq!(store.len(), 1 as usize);
+    fn test_add_dept_emp() {
+        let depts: Vec<Department> = vec!["business", "development", "marketing"];
+        let emps: Vec<Employee> = vec!["Emp 1", "Emp 2", "Emp 3"];
+        let mut _listings: Listings = HashMap::new();
+        let mut i: usize = 0;
+        let mut j: usize = 0;
+        for d in depts {
+            while j <= i {
+                println!("\nadd_dept_emp({:?}, {:?}, {:?})", d, emps[j], _listings);
+                add_dept_emp(d, emps[j], &mut _listings);
+// @todo count the individual entries in each hash_set
+                j += 1;
+            }
+            assert_eq!(_listings.len(), i + 1);
+            j = 0;
+            i += 1;
+        }
     }
-
-    #[test]
-    fn test_get_dept_directory() {}
-
-    #[test]
-    fn test_get_company_directory() {}
 }
