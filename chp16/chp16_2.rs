437000fc30c4b3e08e8c37934a38856fc5960efd
elycruz <603428+elycruz@users.noreply.github.com>
2020-07-24 23:14:40 -0400

    progress on 16.1.

---
+++ chp16/chp16_2.rs
@@ -0,0 +1,97 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn watch_values_arrive() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn send_with_possible_move() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // println!("val is {}", val);
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+fn cloning_channel_senders() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn main() {
+    let (_sender, _receiver) = mpsc::channel();
+
+    thread::spawn(move || {
+        let message = String::from("Geronimo!!!!!");
+        _sender.send(message).unwrap();
+    });
+
+    // Blocks until message received
+    let received = _receiver.recv().unwrap();
+    println!("\nReceived: {}", received);
+
+    // Blocks main thread
+    send_with_possible_move();
+
+    // Blocks main thread
+    watch_values_arrive();
+
+    // Blocks main thread
+    cloning_channel_senders();
+}
